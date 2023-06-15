31a0c1bb3854a3c5f471c603f160e4134c2d7021
Aleksandar Djuran <djuran89@gmail.com>
2023-06-15 20:39:07 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 module.exports = function (RED) {
-	const html2canvas = require("html2canvas");
+	const puppeteer = require("puppeteer");
 
 	function output(config) {
 		const node = this;
@@ -9,17 +9,37 @@ module.exports = function (RED) {
 		node.on("input", async function (msg) {
 			try {
 				const html = config.html;
-				const canvas = await html2canvas(html);
-				const imageBase64 = canvas.toDataURL("image/png");
-
+				const imageBase64 = await convertHtmlToImage(html);
 				msg.payload = imageBase64;
-				node.status({});
+
 				node.send(msg);
 			} catch (err) {
 				node.status({ fill: "red", shape: "dot", text: err });
 				node.error(err, msg);
 			}
 		});
+	}
+
+	async function convertHtmlToImage(htmlString) {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+
+		// Set the viewport size as per your requirements
+		await page.setViewport({ width: 800, height: 600 });
+
+		// Set the HTML content
+		await page.setContent(htmlString);
+
+		// Take a screenshot of the page
+		const screenshotBuffer = await page.screenshot({ type: "png" });
+
+		// Close the browser
+		await browser.close();
+
+		// Convert the screenshot buffer to Base64
+		const dataUrl = screenshotBuffer.toString("base64");
+
+		return dataUrl;
 	}
 
 	RED.nodes.registerType("html-2-image", output);
